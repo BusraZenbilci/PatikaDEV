dc6f14965ba2dd4e23d0e3ad6fb827888fcda5d8
Büşra Zenbilci <88310614+BusraZenbilci@users.noreply.github.com>
2022-11-17 12:08:06 +0300

    Add files via upload

---
+++ VucutKitleIndeksiHesaplama/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        // Scanner tanimla
+        System.out.print("Lütfen boyunuzu (metre cinsinden) giriniz: ");
+        double height = input.nextDouble();
+        // kullanicidan boyunu girmesini iste
+        System.out.print("Lütfen kilonuzu giriniz: ");
+        int kilogram = input.nextInt();
+        // kullanicidan kilosunu girmesini iste
+
+        double bodyMassIndex = kilogram / (height * height);
+        // boy ve kiloya göre vücut kitle indeksini hesapla
+
+        System.out.println("Vucüt kitle indeksiniz: " + bodyMassIndex);
+        // vücut kitle indeksini yazdir.
+    }
+}
